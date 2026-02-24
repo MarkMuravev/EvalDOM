e38a77644f8b1e5839234da77856a2b96d454ead
EchoSystem <echosystem.ru@gmail.com>
2026-02-25 01:30:31 +0300

    -

--- dist/evaldom.umd.js
+++ dist/evaldom.umd.js
@@ -5,17 +5,24 @@
 })(this, (function (exports) { 'use strict';
 
     const DOM = (() => {
+        const nodes = [];
         const evaluate = () => {
             if (window && window.document) {
-                const result = document.evaluate("//*[contains(text(), '{{') and contains(text(), '}}')]", document, null, XPathResult.ANY_TYPE, null);
-                const nodes = [];
-                while ((node = result.iterateNext())) nodes.push(node);
-
-                nodes.forEach(node => {
-                    const html = node.outerHTML;
+                const result = window.document.evaluate("//*[contains(text(), '{{') and contains(text(), '}}')]", document, null, XPathResult.ANY_TYPE, null);
+                let node = null;
+                while ((node = result.iterateNext())) {
+                    const html = String(node.outerHTML);
                     const expression = html.substring(html.indexOf('{{'), html.indexOf('}}') + 2);
                     const code = expression.substring(expression.indexOf('{{') + 2, expression.indexOf('}}'));
-                    node.outerHTML = html.replace(expression, eval(code));
+                    const data = html.split(expression);
+                    nodes.push({ current: node, html: data, code });
+                }
+
+                nodes.forEach((node) => {
+                    const htmlString = node.html[0] + eval(node.code) + node.html[1];
+                    const updated = new DOMParser().parseFromString(htmlString, 'text/html').body.firstChild;
+                    node.current.replaceWith(updated);
+                    node.current = updated;
                 });
             }
         };
